0dd8d3593f66a71a6380fe96395af21b68397a77
Krist Ponpairin <krist7599555@gmail.com>
2022-12-02 11:36:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@ rm -rf "$installed_dir"
 git clone git@github.com:krist7599555/x.git "$installed_dir"
 
 if ! [ "$PATH" =~ "$installed_dir" ]; then
+    echo "Adding PATH $installed_dir to .zshrc"
     export PATH="$installed_dir:$PATH"
     echo "export PATH=\"$installed_dir:\$PATH\"" >> .zshrc
+    echo "Adding PATH Done"
+else
+    echo "PATH ~/.x already exists"
 fi 
 
 x version
