d120f8cb495f9a67d6ff12d888387637c0019ad7
Krist Ponpairin <krist7599555@gmail.com>
2022-12-02 11:37:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ installed_dir="$HOME/.x"
 rm -rf "$installed_dir"
 git clone git@github.com:krist7599555/x.git "$installed_dir"
 
-if ! [ "$PATH" =~ "$installed_dir" ]; then
+if [[ ! "$PATH" =~ "$installed_dir" ]]; then
     echo "Adding PATH $installed_dir to .zshrc"
     export PATH="$installed_dir:$PATH"
     echo "export PATH=\"$installed_dir:\$PATH\"" >> .zshrc
