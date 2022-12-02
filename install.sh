e1d252590026f1827bcfdc611945a4c429be866a
Krist Ponpairin <krist7599555@gmail.com>
2022-12-02 11:42:42 +0700

    fix: path check

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@ installed_dir="$HOME/.x"
 rm -rf "$installed_dir"
 git clone git@github.com:krist7599555/x.git "$installed_dir"
 
-if [[ ! "$PATH" =~ "$installed_dir" ]]; then
+if [[ :$PATH: == *:"$installed_dir":* ]] ; then
+    echo "PATH ~/.x already exists"
+else
     echo "Adding PATH $installed_dir to .zshrc"
     export PATH="$installed_dir:$PATH"
     echo "export PATH=\"$installed_dir:\$PATH\"" >> .zshrc
     echo "Adding PATH Done"
-else
-    echo "PATH ~/.x already exists"
 fi 
 
 x version
